93654e086dd537afa8c816d18f883d7dee6ad921
Juma Antony <jumaantony840@gmail.com>
2023-03-09 15:41:40 +0300

    renamed a couple of files to jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+import {close, logo, menu } from '../assets'
+import { navlinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='w-full flex py-6 justify-between items-center navbar'>
+        hello
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
