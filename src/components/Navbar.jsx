c5ab2c3a9ba1a537914811b7379d76fdd795db33
Juma Antony <jumaantony840@gmail.com>
2023-03-10 08:13:24 +0300

    finished navbar and tweaked a few files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,64 @@
 import React, {useState} from 'react'
 
 import {close, logo, menu } from '../assets'
-import { navlinks } from '../constants'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
+  // Toggler
+  const [toggle, setToggle] = useState(false)
+
   return (
     <div>
       <nav className='w-full flex py-6 justify-between items-center navbar'>
-        hello
+
+        <img src={logo} alt='hoobang' className='w-[124px] h-[32px]'/>
+
+        {/* Desktop Navbar */}
+        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+          
+          {navLinks.map((nav, index) =>(
+            <li key={nav.id}
+              className={`font-poppins text-white font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? `mr-0` : 'mr-10'}`}>
+
+                <a href={`#${nav.id}`} className={``}> 
+                  {nav.title} 
+                </a>
+
+              </li>
+          ))}
+
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img 
+              src={toggle ? close : menu} 
+              alt='toggler'
+              classname='w-[28px] h-[28px] object-contain cursor-pointer'
+              onClick={() => setToggle((prev) => !prev)}
+              />
+
+              <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                  
+                  <ul className='list-none flex justify-end items-center flex-1 flex-col'>
+                    {navLinks.map((nav, index) =>(
+                      <li key={nav.id}
+                        className={`font-poppins text-white font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? `mr-0` : 'mb-4'}`}>
+
+                          <a href={`#${nav.id}`} 
+                          className={``}> 
+                            {nav.title} 
+                          </a>
+
+                        </li>
+
+                    ))}
+                  </ul>
+
+              </div>
+        </div>
       </nav>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
